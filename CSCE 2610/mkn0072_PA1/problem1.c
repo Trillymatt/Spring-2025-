384920de3176863394dc956006d5d9b9d7ded353
Matthew Norman <normanmatthew38@gmail.com>
2025-01-29 16:41:30 -0600

    Comment out code
    This is what the professor asked for.

--- CSCE 2610/mkn0072_PA1/problem1.c
+++ CSCE 2610/mkn0072_PA1/problem1.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int main() {
-    //intializing variables
-    int y = 0, a = 0, n = 0;
-    int result = 0;
-    //Ask user for input and number of iterations
-    printf("Please input the number you would like to add: \n");
-    scanf("%d", &a);
-    printf("Please input the number of iterations you would like: \n");
-    scanf("%d", &n);
-    //Process for summation
-    for(int i = 0; i < (n - 1); i++){
-        if (i == 0) {
-            y = a + a;
-        }
-        else {
-            y = y + a;
-        }
-    }
-    printf("The result of the summation is %d", y);
+// int main() {
+//     //intializing variables
+//     int y = 0, a = 0, n = 0;
+//     int result = 0;
+//     //Ask user for input and number of iterations
+//     printf("Please input the number you would like to add: \n");
+//     scanf("%d", &a);
+//     printf("Please input the number of iterations you would like: \n");
+//     scanf("%d", &n);
+//     //Process for summation
+//     for(int i = 0; i < (n - 1); i++){
+//         if (i == 0) {
+//             y = a + a;
+//         }
+//         else {
+//             y = y + a;
+//         }
+//     }
+//     printf("The result of the summation is %d", y);
 
-    return 0;
-}
+//     return 0;
+// }
