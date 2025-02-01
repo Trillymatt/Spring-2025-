7abb674bc209428a2db2c354943060c862edf26a
Matthew Norman <normanmatthew38@gmail.com>
2025-01-31 23:05:16 -0600

    File probelm 3
    Problem 3 of the programming assignment 1

---
+++ CSCE 2610/mkn0072_PA1/problem3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //Variables
+    int b = 0, n = 0, pow = 0, result = 1;
+    //Input values
+    printf("Input value of b: \n");
+    scanf("%d", &b);
+    printf("Input value of n: \n");
+    scanf("%d", &n);
+    //Compute b to the power of n
+    pow = b;
+    for(int i = 0; i < (n - 1); i++) {
+        if (n == 0) {
+            b = 1;
+        }
+        if (n == 1) {
+            break;
+        }
+        pow = pow * b;
+    }
+    //Compute factorial
+    for(int j = 1; j <= pow; j++) {
+        result *= j;
+    }
+
+    printf("The result is %d", result);
+
+    return 0;
+}
